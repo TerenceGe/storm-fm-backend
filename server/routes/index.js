c577e0489a7774e38320bd7869a3d560a8328c29
Terence Ge <Terencegehui@yahoo.com>
2016-11-10 22:16:25 +0800

    add me router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,17 @@
 import express from 'express'
 import expressJwt from 'express-jwt'
 import authRoutes from './auth'
+import meRoutes from './me'
 import userRoutes from './user'
 import trackRoutes from './track'
-import likeRoutes from './like'
-// import commentRoutes from './comment'
 import config from '../../config/env'
 
 const router = express.Router() // eslint-disable-line new-cap
 
 router.get('/hello', (req, res) => res.send('goodbye'))
 router.use('/auth', authRoutes)
+router.use('/me', meRoutes)
 router.use('/users', userRoutes)
 router.use('/tracks', trackRoutes)
-router.use('/likes', likeRoutes)
-// router.use('/comments', commentRoutes)
 
 export default router
